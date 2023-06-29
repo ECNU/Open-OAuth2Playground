aeb76c842b265870a136cdc8d1a367b94ab7fb2e
onetrue5200 <oneruexie@gmail.com>
2023-06-29 14:42:49 +0800

    impl api

--- controller/oauth2_api.go
+++ controller/oauth2_api.go
@@ -1,6 +1,12 @@
 package controller
 
 import (
+	"encoding/base64"
+	"net/http"
+	"strconv"
+
+	"github.com/ECNU/Open-OAuth2Playground/g"
+	"github.com/ECNU/Open-OAuth2Playground/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,5 +19,44 @@ type ReqApiData struct {
 	HttpBody        string            `json:"http_body"`
 }
 
+func base64Decode(input string) (string, error) {
+	decodedBytes, err := base64.StdEncoding.DecodeString(input)
+	if err != nil {
+		return "", err
+	}
+	decodedString := string(decodedBytes)
+	return decodedString, nil
+}
+
 func api(c *gin.Context) {
+	reqData := ReqApiData{}
+	if err := c.Bind(&reqData); err != nil {
+		c.JSON(http.StatusOK, handleError(err.Error()))
+		return
+	}
+	method := reqData.Method
+	apiAddr := reqData.ApiAddr
+	header := reqData.Header
+	body, err := base64Decode(reqData.HttpBody)
+	if err != nil {
+		c.JSON(http.StatusOK, handleError(err.Error()))
+		return
+	}
+	header["Content-Length"] = strconv.Itoa(len(body))
+
+	if reqData.AccessTokenType == "bearer" {
+		header["Authorization"] = "Bearer " + reqData.AccessToken
+	} else if reqData.AccessTokenType == "query" {
+		apiAddr += "?access_token=" + reqData.AccessToken
+	} else {
+		c.JSON(http.StatusOK, handleError("invalid access token"))
+		return
+	}
+
+	res, err := models.HandleRequest(method, apiAddr, g.UserAgent, body, g.Config().Timeout, header)
+	if err != nil {
+		c.JSON(http.StatusOK, handleError(err.Error()))
+		return
+	}
+	c.JSON(http.StatusOK, handleSuccess(res))
 }
